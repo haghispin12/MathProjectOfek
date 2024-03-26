c655b293a139c7d9a4e8d64247cdc1c29e5dde1d
hag <hag.hispin12@gmail.com>
2024-03-26 18:49:28 +0200

    continue fragment NOT FINISH

--- app/src/main/java/com/example/mathprojectofek/fragment_showusers.java
+++ app/src/main/java/com/example/mathprojectofek/fragment_showusers.java
@@ -7,24 +7,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  .
  */
 public class fragment_showusers extends Fragment {
+    private EditText user;
+    private Button addPicture;
+    private Button addUser;
 
-
-
+    MainViewModel mainViewModel;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        }
 
+    }
+
+    private void initView(View view) {
+        user =view.findViewById(R.id.user);
+        addPicture=view.findViewById(R.id.addPicture);
+        addUser=view.findViewById(R.id.addUser);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_showusers, container, false);
+        View view = inflater.inflate(R.layout.fragment_showusers, container, false);
+
+        initView(view);
+        return  view;
     }
 }
