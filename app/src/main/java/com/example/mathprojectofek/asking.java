80a22a0301d749f906cc0ce98752573577afa101
hag <hag.hispin@gmail.com>
2025-04-23 15:32:11 +0300

    continue the real project

--- app/src/main/java/com/example/mathprojectofek/asking.java
+++ app/src/main/java/com/example/mathprojectofek/asking.java
@@ -1,9 +1,6 @@
 package com.example.mathprojectofek;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.firestore.FirebaseFirestore;
+public class asking extends AppCompatActivity {
 
-
-public class MainActivity extends AppCompatActivity {
-private EditText email;
-private EditText name;
-private Button connect;
-private Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseFirestore db= FirebaseFirestore.getInstance();     //FirebaseFirestore.getInstance();
-        Log.d("","");
         EdgeToEdge.enable(this);
-        initView();
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_asking);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    private void initView() {
-        email=findViewById(R.id.email);
-        name=findViewById(R.id.name);
-        connect=findViewById(R.id.connect);
-        register=findViewById(R.id.register);
     }
 }
