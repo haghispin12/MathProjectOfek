f46f22824b7b2fbe942ec06ac40f064969daa2f7
hag <hag.hispin@gmail.com>
2025-05-04 14:53:22 +0300

    finish all the spinners

--- app/src/main/java/com/example/mathprojectofek/asking.java
+++ app/src/main/java/com/example/mathprojectofek/asking.java
@@ -2,6 +2,7 @@ package com.example.mathprojectofek;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,9 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -34,6 +40,8 @@ public class asking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asking);
+        ArrayList<String> students = new ArrayList<String>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         EdgeToEdge.enable(this);
         initView();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -41,11 +49,25 @@ public class asking extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        loadStudents();
+        CollectionReference studentsRef = db.collection("students");
+        studentsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        // Access specific fields
+                        String name = document.getString("name");
+                        students.add(name);
+                    }
+                    Log.d("","");
+                    loadStudents(students);
+                }
+            }
+        });
+
     }
 
 
-    @SuppressLint("WrongViewCast")
     private void initView() {
         Name = findViewById(R.id.Name);
         Spinner1 = findViewById(R.id.Spinner1);
@@ -53,38 +75,77 @@ public class asking extends AppCompatActivity {
         Spinner3 = findViewById(R.id.Spinner3);
         Spinner4 = findViewById(R.id.Spinner4);
         Spinner5 = findViewById(R.id.Spinner5);
+        String name;
     }
-    private void loadStudents() {
-        List<String> students = new ArrayList<>();
-        FirebaseFirestore.getInstance().collection("students").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+
+    private void loadStudents(ArrayList<String>students) {
+        ArrayAdapter<String>adapter=new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,students);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Name.setAdapter(adapter);
+        Name.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                    if (documentSnapshot.exists()) {
-                        String name = documentSnapshot.getString("name");
-                        students.add(name);
-                    }
-                }
-                // Now that we have the data, update the spinner
-                updateNameSpinner(students);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
-    }
-    private void updateNameSpinner(List<String> students) {
-        // Create an ArrayAdapter using the list of student names
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_spinner_item,
-                students
-        );
-
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        Name.setAdapter(adapter);
+        Spinner1.setAdapter(adapter);
+        Spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice1 = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(asking.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+        Spinner2.setAdapter(adapter);
+        Spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice2 = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(asking.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+        Spinner3.setAdapter(adapter);
+        Spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice3 = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(asking.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+        Spinner4.setAdapter(adapter);
+        Spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice4 = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(asking.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+        Spinner5.setAdapter(adapter);
+        Spinner5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice5 = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(asking.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
     }
 }
+
 
 
 //    private void initView(ArrayList<String>students) {
